d7ce85949c1127e7b52e480e95dfada811b29566
kingsley Arinze <kingsleyarinzeugwu@gmail.com>
2024-09-01 10:10:08 -0700

    complete model, controller and routes

---
+++ app.js
@@ -0,0 +1,32 @@
+//import the required dependencies
+const express = require("express");
+const db = require("./config/dbConfig");
+require("./models/PatientModel");
+const cors = require("cors");
+const morgan = require("morgan");
+
+//create express app
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(morgan("tiny"));
+const port = process.env.PORT || 5000;
+
+//route
+app.use("/EMS/patients", require("./routes/patientRoute"));
+app.use("/EMS/staff", require("./routes/StaffDataController"));
+
+
+
+const start = () => {
+  try {
+    app.listen(port, () => {
+      console.log(`app is listening to port ${port}`);
+    });
+  } catch (error) {}
+};
+
+start();
